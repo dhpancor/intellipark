fa1d6084877e734adc66af3160629e849ab1b395
Daniel Hernández <dhernandez0798@gmail.com>
2020-05-19 10:31:25 +0200

    updated favicon and footer text

--- client/src/app/@theme/components/footer/footer.component.ts
+++ client/src/app/@theme/components/footer/footer.component.ts
@@ -4,12 +4,10 @@ import {Component} from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">IntelliPark - Crafted with ♥ by <b><a href="https://github.com/dhernandez0798" target="_blank">@dhernandez0798</a></b> 2020.</span>
+    <span class="created-by">IntelliPark &copy; 2020</span>
     <div class="socials">
-      <!--<a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>-->
+      <a href="https://github.com/dhernandez0798" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://www.linkedin.com/in/dhernandez0798/" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
