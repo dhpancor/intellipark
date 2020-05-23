ca2a27e6957c2ac0dba5e9d04400593a92f08079
Daniel Hernández <dhernandez0798@gmail.com>
2020-05-24 01:14:44 +0200

    added actions menu

--- client/src/app/pages/clients/clients.module.ts
+++ client/src/app/pages/clients/clients.module.ts
@@ -1,7 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ClientsListComponent} from "./clients-list/clients-list.component";
-import {NbButtonModule, NbCardModule, NbIconModule, NbInputModule, NbSelectModule} from "@nebular/theme";
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbIconModule,
+  NbInputModule,
+  NbSelectModule
+} from "@nebular/theme";
 import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 import {RouterModule} from "@angular/router";
 import {ClientsFormComponent} from "./clients-form/clients-form.component";
@@ -9,16 +16,17 @@ import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [ClientsListComponent, ClientsFormComponent],
-    imports: [
-        CommonModule,
-        NbCardModule,
-        NgxDatatableModule,
-        NbInputModule,
-        NbSelectModule,
-        NbButtonModule,
-        RouterModule,
-        ReactiveFormsModule,
-        NbIconModule
-    ]
+  imports: [
+    CommonModule,
+    NbCardModule,
+    NgxDatatableModule,
+    NbInputModule,
+    NbSelectModule,
+    NbButtonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    NbIconModule,
+    NbActionsModule
+  ]
 })
 export class ClientsModule { }
