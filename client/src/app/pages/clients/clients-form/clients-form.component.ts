54796ddfc3709835e7c6be79544c33d12742d98a
Daniel Hernández <dhernandez0798@gmail.com>
2020-05-24 00:14:49 +0200

    handling form errors

--- client/src/app/pages/clients/clients-form/clients-form.component.ts
+++ client/src/app/pages/clients/clients-form/clients-form.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {ClientsService} from "../../../providers/clients.service";
 import {Client} from "../../../models/client";
 import {ActivatedRoute, Router} from "@angular/router";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {log} from "util";
 import {NbToastrService} from "@nebular/theme";
 
@@ -18,12 +18,12 @@ export class ClientsFormComponent implements OnInit {
 
   client: Client = null;
   form: FormGroup = this.fb.group({
-    dni: [''],
-    first_name: [''],
-    last_name: [''],
+    dni: ['', [Validators.required, Validators.minLength(8)]],
+    first_name: ['', [Validators.required, Validators.minLength(2)]],
+    last_name: ['', [Validators.required, Validators.minLength(4)]],
     comments: [''],
-    email: [''],
-    gender: [''],
+    email: ['', [Validators.required, Validators.email]],
+    gender: ['', [Validators.required]],
   });
 
   ngOnInit(): void {
@@ -32,17 +32,19 @@ export class ClientsFormComponent implements OnInit {
   }
 
   onFormSubmit(): void {
-    this.client.dni = this.form.get('dni').value;
-    this.client.first_name = this.form.get('first_name').value;
-    this.client.last_name = this.form.get('last_name').value;
-    this.client.email = this.form.get('email').value;
-    this.client.gender = this.form.get('gender').value;
-    this.client.comments = this.form.get('comments').value;
+    if (this.form.valid && this.form.dirty) {
+      this.client.dni = this.form.get('dni').value;
+      this.client.first_name = this.form.get('first_name').value;
+      this.client.last_name = this.form.get('last_name').value;
+      this.client.email = this.form.get('email').value;
+      this.client.gender = this.form.get('gender').value;
+      this.client.comments = this.form.get('comments').value;
 
-    if (this.route.snapshot.params.id === 'new') {
-      this.clientService.create(this.client).subscribe(_ => this.successfulOperation());
-    } else {
-      this.clientService.update(this.client).subscribe(_ => this.successfulOperation());
+      if (this.route.snapshot.params.id === 'new') {
+        this.clientService.create(this.client).subscribe(_ => this.successfulOperation());
+      } else {
+        this.clientService.update(this.client).subscribe(_ => this.successfulOperation());
+      }
     }
   }
 
--- client/src/app/pages/clients/clients.module.ts
+++ client/src/app/pages/clients/clients.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ClientsListComponent} from "./clients-list/clients-list.component";
-import {NbButtonModule, NbCardModule, NbInputModule, NbSelectModule} from "@nebular/theme";
+import {NbButtonModule, NbCardModule, NbIconModule, NbInputModule, NbSelectModule} from "@nebular/theme";
 import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 import {RouterModule} from "@angular/router";
 import {ClientsFormComponent} from "./clients-form/clients-form.component";
@@ -17,7 +17,8 @@ import {ReactiveFormsModule} from "@angular/forms";
         NbSelectModule,
         NbButtonModule,
         RouterModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        NbIconModule
     ]
 })
 export class ClientsModule { }
