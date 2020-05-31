91b91ed3a8a24cad7598e39cd2cdcf29a9e4fade
Daniel Hernández <dhernandez0798@gmail.com>
2020-05-31 16:03:59 +0200

    added migration with dummy data seeds

--- ts-backend/src/index.ts
+++ ts-backend/src/index.ts
@@ -6,7 +6,7 @@ import * as cors from 'cors';
 import * as helmet from 'helmet';
 import routes from './routes';
 
-createConnection().then(async () => {
+createConnection().then(async connection => {
   // create express app
   const app = express();
   app.use(bodyParser.json());
@@ -20,6 +20,9 @@ createConnection().then(async () => {
 
   // start express server
   app.listen(3000);
+
+  // Uncomment line below to insert dummy test data
+  // await connection.runMigrations();
 
   console.log('TS-Backend started on http://localhost:3000');
 }).catch((error) => console.log(error));
