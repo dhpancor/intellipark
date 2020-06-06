b1c41a1543ed7c988036bded1c8792ca393b8819
Daniel Hernández <dhernandez0798@gmail.com>
2020-06-06 12:39:18 +0200

    used querybuilder to get last accesses

--- ts-backend/src/controller/ClientsController.ts
+++ ts-backend/src/controller/ClientsController.ts
@@ -3,6 +3,7 @@ import { Client } from '../entity/Client';
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { JsonResponse } from './utils/JsonResponse';
+import { AccessLog } from '../entity/AccessLog';
 
 export class ClientsController extends BaseCRUD {
   constructor () {
@@ -12,7 +13,18 @@ export class ClientsController extends BaseCRUD {
   getLastAccesses = async (request: Request, response: Response) => {
     let data = null;
     try {
-      data = await getRepository(Client).findOne(request.params.id, { relations: ['vehicle', 'vehicle.accessLogs'] });
+      // data = await getRepository(Client).findOne(request.params.id, { relations: ['vehicle', 'vehicle.accessLogs'] });
+      data = await getRepository(AccessLog)
+        .createQueryBuilder('accesslog')
+        .innerJoin('accesslog.vehicle', 'vehicle')
+        .innerJoin('vehicle.client', 'client')
+        .select('accesslog')
+        .addSelect(['vehicle.plate', 'vehicle.id'])
+        .where('client.id = :clientId')
+        .orderBy('accesslog.id', 'DESC')
+        .setParameters({ clientId: request.params.id })
+        .take(10)
+        .getMany();
     } catch (e) {
       console.log(e);
       return response.send(new JsonResponse('Fatal error. Try again later.', false));
