3dd4fb059c2f38364fa468414ed6da97e2934c1f
Daniel Hernández <dhernandez0798@gmail.com>
2020-06-11 11:02:25 +0200

    Added Bcrypt for user passwords

--- ts-backend/src/controller/AuthController.ts
+++ ts-backend/src/controller/AuthController.ts
@@ -3,14 +3,19 @@ import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
 import { JsonResponse } from './utils/JsonResponse';
 import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
 
 export class AuthController {
   static login = async (request: Request, response: Response) => {
     const repository = getRepository(User);
     try {
-      const user = await repository.findOneOrFail({ email: request.body.email, password: request.body.password });
-      const token = jwt.sign({ id: user.id }, 'test123', { expiresIn: '30d' });
-      return response.send({ token });
+      const user = await repository.findOneOrFail({ email: request.body.email }, { select: ['id', 'password'] });
+      if (await bcrypt.compare(request.body.password, user.password)) {
+        const token = jwt.sign({ id: user.id }, 'test123', { expiresIn: '30d' });
+        return response.send({ token });
+      } else {
+        return response.status(401).send(new JsonResponse('Credentials not valid', false));
+      }
     } catch (e) {
       return response.status(401).send(new JsonResponse('Credentials not valid', false));
     }
--- ts-backend/src/controller/UsersController.ts
+++ ts-backend/src/controller/UsersController.ts
@@ -2,10 +2,36 @@ import { BaseCRUD } from './BaseCRUD';
 import { Request, Response } from 'express';
 import { JsonResponse } from './utils/JsonResponse';
 import { User } from '../entity/User';
+import { getRepository, QueryFailedError } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 export class UsersController extends BaseCRUD {
   constructor () {
     super(User);
+  }
+
+  save = async (request: Request, response: Response) => {
+    const repository = getRepository(this.entityClass);
+    let data = null;
+    try {
+      const user = new User();
+      if (request.body.newPassword) {
+        user.password = await bcrypt.hash(request.body.newPassword, 10);
+      }
+      // Cleanup the request body
+      if (request.body.password) {
+        delete request.body.password;
+      }
+      Object.assign(user, request.body);
+      data = await repository.save(user);
+    } catch (e) {
+      if (e instanceof QueryFailedError) {
+        return response.send(new JsonResponse('Values submitted were not valid.', false));
+      } else {
+        return response.send(new JsonResponse('Fatal error. Try again later.', false));
+      }
+    }
+    return response.send(new JsonResponse('User saved.'));
   }
 
   whoAmI = async (request: Request, response: Response) => {
